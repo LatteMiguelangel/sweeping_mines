3a8e2998044e8bb5dd2a210b6864dda00b59a0ab
LatteMiguelangel <leonet.miguelangel.2021@gmail.com>
2025-06-26 20:59:55 -0400

    Grid con Imagen

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,17 +1,17 @@
 class Assets {
   static const String _path = "assets/images";
   static const String cellBomb = "$_path/cell_bomb.png";
-  static const String cellClosed = "$_path/cell_closed";
-  static const String cellFlagged = "$_path/cell_flagged";
-  static const String cellOpened = "$_path/cell_opened";
-  static const String cellOpened1 = "$_path/cell_opened_1";
-  static const String cellOpened2 = "$_path/cell_opened_2";
-  static const String cellOpened3 = "$_path/cell_opened_3";
-  static const String cellOpened4 = "$_path/cell_opened_4";
-  static const String cellOpened5 = "$_path/cell_opened_5";
-  static const String cellOpened6 = "$_path/cell_opened_6";
-  static const String cellOpened7 = "$_path/cell_opened_7";
-  static const String cellOpened8 = "$_path/cell_opened_8";
+  static const String cellClosed = "$_path/cell_closed.png";
+  static const String cellFlagged = "$_path/cell_flagged.png";
+  static const String cellOpened = "$_path/cell_opened.png";
+  static const String cellOpened1 = "$_path/cell_opened_1.png";
+  static const String cellOpened2 = "$_path/cell_opened_2.png";
+  static const String cellOpened3 = "$_path/cell_opened_3.png";
+  static const String cellOpened4 = "$_path/cell_opened_4.png";
+  static const String cellOpened5 = "$_path/cell_opened_5.png";
+  static const String cellOpened6 = "$_path/cell_opened_6.png";
+  static const String cellOpened7 = "$_path/cell_opened_7.png";
+  static const String cellOpened8 = "$_path/cell_opened_8.png";
 
   static const List<String> openedCells = [
     cellBomb,
